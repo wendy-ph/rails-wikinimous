b37ef5696531da59d58944d77bee942524765e3f
Wendy Pham <wendypham_s2@hotmail.com>
2022-04-09 14:41:47 -0400

    added edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  # before_action :find_article
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -9,14 +9,36 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  # def create
-  #   @article = Article.new(article_params)
-  # end
+  def show
+  end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_path
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+
+  end
 
   private
 
   def find_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
