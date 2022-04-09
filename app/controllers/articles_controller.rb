f0dd1aed4ad2a611f8f4293af24deb067ea8b589
Wendy Pham <wendypham_s2@hotmail.com>
2022-04-09 14:24:50 -0400

    added 'new' page and form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,22 @@
 class ArticlesController < ApplicationController
+  # before_action :find_article
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  # def create
+  #   @article = Article.new(article_params)
+  # end
+
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
 end
